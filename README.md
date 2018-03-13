a5bf9e306fadb5f0eb0b27220ed3d9817fcf32ce
Franky Lau <killalau1988@hotmail.com>
2018-03-13 17:15:53 +0800

    feat(server): basic koa server

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Node Docker
-
+# koa boilerplate
---
+++ src/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const bunyan = require('bunyan');
+const Koa = require('koa');
+const Qs = require('koa-qs');
+const bodyParser = require('koa-bodyparser');
+const Router = require('koa-router');
+const koaLogger = require('koa-bunyan');
+
+// server
+const app = new Koa();
+Qs(app);
+app.use(bodyParser());
+
+// logger
+const logger = bunyan.createLogger({
+  name: 'koa-boilerplate',
+  streams: [
+    { stream: process.stdout, level: 'debug' },
+  ],
+});
+app.use(koaLogger(logger, { level: 'debug' }));
+
+// router
+const router = new Router();
+router.get('/hello', async (ctx) => {
+  ctx.body = 'world';
+});
+app.use(router.routes())
+  .use(router.allowedMethods());
+
+// start server
+const port = process.env.PORT || 3000;
+logger.info(`Server start listening on port ${port}`);
+app.listen(port);
